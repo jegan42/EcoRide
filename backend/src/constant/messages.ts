9f32cb0ab656c4b39055963c386f36d4d4e2ba75
jegan42 <aznchristophe@gmail.com>
2025-05-22 01:45:59 +0200

    fix: change messages and add error on response

--- backend/src/constant/messages.ts
+++ backend/src/constant/messages.ts
@@ -3,14 +3,14 @@ export const messages = {
   SUCCESS: (method: string, item: string): string =>
     `Successfully ${method}: ${item}`,
   SUCCESS_CREATE: (method: string, item: string): string =>
-    `Successfully ${method}: ${item}`,
+    `Successfully created ${method}: ${item}`,
 
   BAD_REQUEST: (method: string, item: string): string =>
     `Bad request ${method}: ${item}`,
   UNAUTHORIZED: (method: string, item: string): string =>
-    `Authentication ${method}: ${item}`,
+    `Unauthorized access ${method}: ${item}`,
   FORBIDDEN: (method: string, item: string): string =>
-    `Access ${method}: ${item}`,
+    `Access denied ${method}: ${item}`,
   NOT_FOUND: (method: string, item: string): string =>
     `Not found ${method}: ${item}`,
   CONFLICT: (method: string, item: string): string =>
@@ -18,7 +18,8 @@ export const messages = {
   LIMIT_EXCEEDED: (method: string, item: string): string =>
     `Limit exceeded ${method}: ${item}`,
 
-  ERROR: (method: string, item: string): string => `Error ${method}: ${item}`,
+  ERROR: (method: string, item: string): string =>
+    `Internal error ${method}: ${item}`,
 };
 
 export type Message = (typeof messages)[keyof typeof messages];
