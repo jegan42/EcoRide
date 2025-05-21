6d9be487118dab3b7cf93f7169a1e8f9bd8a909a
jegan42 <aznchristophe@gmail.com>
2025-05-21 18:59:36 +0200

    fix: reduce messages and status

--- backend/src/constant/messages.ts
+++ backend/src/constant/messages.ts
@@ -5,30 +5,16 @@ export const messages = {
   SUCCESS_CREATE: (method: string, item: string): string =>
     `Successfully ${method}: ${item}`,
 
-  FAILED: (method: string, item: string): string =>
-    `Failed to ${method}: ${item}`,
-  INVALID: (method: string, item: string): string =>
-    `Invalid ${method}: ${item}`,
-  MISSING: (method: string, item: string): string =>
-    `Missing ${method}: ${item}`,
   BAD_REQUEST: (method: string, item: string): string =>
     `Bad request ${method}: ${item}`,
-  REQUIRED: (method: string, item: string): string =>
-    `Required ${method}: ${item}`,
-
   UNAUTHORIZED: (method: string, item: string): string =>
     `Authentication ${method}: ${item}`,
   FORBIDDEN: (method: string, item: string): string =>
     `Access ${method}: ${item}`,
-
   NOT_FOUND: (method: string, item: string): string =>
     `Not found ${method}: ${item}`,
-
-  ALREADY: (method: string, item: string): string =>
-    `Already ${method}: ${item}`,
   CONFLICT: (method: string, item: string): string =>
     `Conflict ${method}: ${item}`,
-
   LIMIT_EXCEEDED: (method: string, item: string): string =>
     `Limit exceeded ${method}: ${item}`,
 
