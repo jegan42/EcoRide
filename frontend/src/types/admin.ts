f8b83ce385a4eea39c05b37e18dadd428831ce68
jegan42 <aznchristophe@gmail.com>
2025-06-25 23:16:58 +0200

    chore: add admin types

---
+++ frontend/src/types/admin.ts
@@ -0,0 +1,15 @@
+// frontemd/src/types/admin.ts
+export type AdminFormMode =
+  | ''
+  | 'userList'
+  | 'contactList'
+  | 'tripList'
+  | 'statList'
+  | 'userEdit'
+  | 'contactEdit'
+  | 'tripEdit'
+  | 'statEdit'
+  | 'userDelete'
+  | 'contactDelete'
+  | 'tripDelete'
+  | 'statDelete';
