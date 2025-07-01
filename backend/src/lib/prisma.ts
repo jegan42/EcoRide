efa814911f3c9210bd92519656649fe94ccf3dfb
jegan42 <aznchristophe@gmail.com>
2025-07-01 16:08:12 +0200

    fix:log database url

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,6 +1,8 @@
 // backend/src/lib/prisma.ts
 import { PrismaClient } from '../../generated/prisma';
 
+console.log('🔍 DATABASE_URL:', process.env.DATABASE_URL);
+
 const prismaNewClient = new PrismaClient();
 
 export default prismaNewClient;
