a2da72f9703abc522da52ef0e19aa91567e281ee
jegan42 <aznchristophe@gmail.com>
2025-07-02 10:17:24 +0200

    fix:add log control

--- backend/generated/prisma/edge.js
+++ backend/generated/prisma/edge.js
@@ -252,7 +252,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -5,4 +5,8 @@ console.log('🔍 DATABASE_URL:', process.env.DATABASE_URL);
 
 const prismaNewClient = new PrismaClient();
 
+prismaNewClient.user.findMany()
+  .then((res) => console.log('✅ DB reachable!', res))
+  .catch((err) => console.error('❌ Cannot reach DB:', err));
+
 export default prismaNewClient;
