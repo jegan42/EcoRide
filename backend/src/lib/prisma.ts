54bc25083cf921b875f27c0323712d5d328dd53d
jegan42 <aznchristophe@gmail.com>
2025-07-02 11:35:56 +0200

    fix:add dnslookup

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,10 @@
 // backend/src/lib/prisma.ts
 import { PrismaClient } from '../../generated/prisma';
+import dns from 'dns/promises';
+
+dns.lookup('db.bfxafnzrvkxesttgjqqk.supabase.co')
+  .then(r => console.log('🔍 DNS resolved:', r))
+  .catch(e => console.error('❌ DNS ERR', e));
 
 console.log('🔍 DATABASE_URL:', process.env.DATABASE_URL);
 
