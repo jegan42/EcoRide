163588ff3dd6b1115742539af87faf3163d14274
jegan42 <aznchristophe@gmail.com>
2025-07-02 10:47:37 +0200

    fix:add log control

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -5,6 +5,9 @@ console.log('🔍 DATABASE_URL:', process.env.DATABASE_URL);
 
 const prismaNewClient = new PrismaClient();
 
+prismaNewClient.$connect().then((res) => console.log('✅ DB connected!', res))
+.catch((err) => console.error('❌ Cannot connect DB:', err));
+
 prismaNewClient.user.findMany()
   .then((res) => console.log('✅ DB reachable!', res))
   .catch((err) => console.error('❌ Cannot reach DB:', err));
