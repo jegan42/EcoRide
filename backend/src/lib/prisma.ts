85ef237dff711fb6640553c03e8c00014fc2fb49
jegan42 <aznchristophe@gmail.com>
2025-07-02 13:16:20 +0200

    fix:add dnslookup

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,18 +2,31 @@
 import { PrismaClient } from '../../generated/prisma';
 import dns from 'dns/promises';
 
-dns.lookup('db.bfxafnzrvkxesttgjqqk.supabase.co')
-  .then(r => console.log('🔍 DNS resolved:', r))
-  .catch(e => console.error('❌ DNS ERR', e));
+dns
+  .lookup('db.bfxafnzrvkxesttgjqqk.supabase.co')
+  .then((r) => console.log('🔍 DNS resolved:', r))
+  .catch((e) => console.error('❌ DNS ERR', e));
+
+async function testLookup() {
+  const res = await dns.lookup('db.bfxafnzrvkxesttgjqqk.supabase.co', {
+    family: 4,
+  });
+  console.log('IPv4 address:', res.address);
+}
+
+testLookup();
 
 console.log('🔍 DATABASE_URL:', process.env.DATABASE_URL);
 
 const prismaNewClient = new PrismaClient();
 
-prismaNewClient.$connect().then((res) => console.log('✅ DB connected!', res))
-.catch((err) => console.error('❌ Cannot connect DB:', err));
+prismaNewClient
+  .$connect()
+  .then((res) => console.log('✅ DB connected!', res))
+  .catch((err) => console.error('❌ Cannot connect DB:', err));
 
-prismaNewClient.user.findMany()
+prismaNewClient.user
+  .findMany()
   .then((res) => console.log('✅ DB reachable!', res))
   .catch((err) => console.error('❌ Cannot reach DB:', err));
 
